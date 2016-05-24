275d9a5d243109859eeaf42cc8cacc8a1b3047d5
shahz <shahz@DESKTOP-1LBMKH3>
2016-05-24 14:16:27 -0400

    made fields and properties

--- Comp123 lesson 3/Program.cs
+++ Comp123 lesson 3/Program.cs
@@ -8,8 +8,22 @@ namespace Comp123_lesson_3
 {
     class Program
     {
+        /** 
+         * main method with person class
+         * @mathod main
+         * @param (string[]) args
+         */ 
         static void Main(string[] args)
         {
+            // create a new instance of person class
+            Person person = new Person();
+            Console.WriteLine(person.Name);
+            // set a value in the Name property of the Person Class
+            person.Name = "Tom";
+            // get the Name value and print to console
+            Console.WriteLine(person.Name);
+            Person person0 = new Person("tommy");
+            
         }
     }
 }
--- Comp123 lesson 3/person.cs
+++ Comp123 lesson 3/person.cs
@@ -6,17 +6,45 @@ using System.Threading.Tasks;
 
 namespace Comp123_lesson_3
 {
-    class person
+    /**
+     * this calss defines a generic person
+     * @class person
+     * @field _name {string}
+     */
+      
+    class Person
     {
         // private instance variables 
         private string _name;
 
         // public properties
+        /**
+         * this is a property for our_name field
+         * 
+         * @proeprty {string} Name
+         */ 
         public string Name
         {
             get { return this._name; }
             set { this._name = value; }
         }
+        /**
+         * this is the default empty constructor for the person class
+         * @constructor person 
+         */
+        public Person()
+        {
+            this._name = "unknown name";
+        }
+        /**
+        * this is the default empty constructor for the person class
+        * @constructor person 
+        */
+        public Person(string name)
+        {
+            this._name = name;
+        }
         
+   
     }
 }
