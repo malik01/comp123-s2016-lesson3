208cb833ad1feb7131ed6b1d5fc5c9de5830efbf
shahz <shahz@DESKTOP-1LBMKH3>
2016-05-24 13:17:45 -0400

    Made a class and added stuff

---
+++ Comp123 lesson 3/person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp123_lesson_3
+{
+    class person
+    {
+        // private instance variables 
+        private string _name;
+
+        // public properties
+        public string Name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
+        
+    }
+}
